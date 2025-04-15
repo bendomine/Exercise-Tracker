c0a0de227a1624906ca6f55b94d45753032c17d9
Ben Domine <pr0gr4mm3rben@gmail.com>
2025-04-15 11:46:59 -0500

    Initial Commit

---
+++ Exercise Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exercise Tracker
+//
+//  Created by Ben Domine on 4/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Exercise Tracker/Exercise_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise_TrackerApp.swift
+//  Exercise Tracker
+//
+//  Created by Ben Domine on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Exercise_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
